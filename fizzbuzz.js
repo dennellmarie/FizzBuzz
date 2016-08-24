388c531f41da139b2f13c0b69db6976f8c545152
dennellmarie <dennell.lucero@gmail.com>
2016-08-24 13:35:39 -0700

    fizzbuzz prints in browser

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+var fizzbuzz = document.getElementById("fizzbuzz");
+
+for (i = 1; i <= 100; i++) {
+	if(i % 3 == 0 && i % 5 == 0) {
+		fizzbuzz.innerHTML += "fizz buzz <br />";
+	} else if(i % 5 == 0) {
+		fizzbuzz.innerHTML += "buzz <br />";
+	} else if(i % 3 == 0) {
+		fizzbuzz.innerHTML += "fizz <br />";
+	} else {
+		fizzbuzz.innerHTML += i + "<br />";
+	}
+}
